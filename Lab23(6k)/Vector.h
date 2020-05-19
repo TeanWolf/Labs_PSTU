36d871414cc4eac950104a3a9048d6520e369547
Minin Valentin <63961721+TeanWolf@users.noreply.github.com>
2020-05-19 21:01:49 +0500

    Add files via upload

---
+++ Lab23(6k)/Vector.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class iteratorF
+{
+	friend class Vector;
+public:
+	iteratorF() { elem = 0; };
+	iteratorF(const iteratorF& p) { elem = p.elem; };
+	bool operator!=(const iteratorF& it) { return elem != it.elem; };
+	void operator--() { --elem; }
+	void operator++() { ++ elem; }
+	int& operator*()const { return*elem; }
+private:
+	int* elem;
+};
+class Vector
+{
+public:
+	Vector(int s);
+	Vector(const Vector& p);
+	~Vector();
+	Vector& operator=(const Vector& p);
+	int& operator[](int index); 
+	int operator()();
+	void operator+(const Vector& p);
+	friend istream& operator>> (istream& in, Vector& p);
+	friend ostream&  operator<<(ostream& out, const Vector& p);
+	Vector operator+(const int k);
+	iteratorF first() { return beg; };
+	iteratorF last() { return end; };
+	iteratorF operator-(int n)
+	{
+		end.elem -= n;
+		cout<<*end.elem;
+		return uk_elem;
+
+	}
+private:
+	int size;
+	int* data;
+	iteratorF beg;
+	iteratorF end;
+	iteratorF uk_elem;
+};
